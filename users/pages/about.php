b61ad964b0206a8b9e9bfab270742094686e9501
chadrhino <chadrhino29@gmail.com>
2024-12-12 13:01:07 +0800

    ur

--- users/pages/about.php
+++ users/pages/about.php
@@ -5,9 +5,48 @@
     <p>The JSV Piggery farm management systems have advanced significantly in recent years, using technology to enhance efficiency, productivity, and sustainability. These systems integrate various components such as automated feeding, environmental control, and health monitoring, offering a comprehensive approach to modern pig farming.</p>
 
     <p>Automated feeding systems have become more sophisticated, ensuring precise and consistent feed delivery. They emphasized that automated feeding not only reduces labor costs but also enhances growth performance and welfare in pigs by keeping consistent feeding schedules and quantities. Additionally, these systems can be customized to meet the specific nutritional needs of different pig groups, such as sows, piglets, and fattening pigs (van der Staay et.al, 2020).</p>
-    <p>Teams</p>
 </div>
 
 <?php
 require 'users/footer.php';
  ?>
+ <style>
+  body {
+    background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.4)), url('https://jsvpiggery.com/users/pages/images/piggery.jpg');
+    background-size: cover; 
+    background-position: center; 
+}
+    .wrap-input100 {
+    position: relative;
+    width: 100%;
+    margin-bottom: 20px;
+  }
+
+  .input100 {
+    font-family: Poppins-Regular;
+    font-size: 16px;
+    color: #333;
+    line-height: 1.2;
+    display: block;
+    width: 100%;
+    background: #e6e6e6;
+    height: 55px;
+    border-radius: 25px;
+    padding: 0 30px 0 68px;
+    box-sizing: border-box;
+  }
+
+  .symbol-input100 {
+    position: absolute;
+    font-size: 18px;
+    color: #999999;
+    top: 50%;
+    left: 35px;
+    transform: translateY(-50%);
+    transition: all 0.4s;
+  }
+  .txt1 {
+    font-size: 14px;
+    color: #999999;
+    line-height: 1.5;
+  }
