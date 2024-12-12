824cdc11ebce68b8e02baf5febc786367431dab5
chadrhino <chadrhino29@gmail.com>
2024-12-12 13:36:23 +0800

    ruu

--- users/pages/about.php
+++ users/pages/about.php
@@ -5,14 +5,11 @@
     <p>The JSV Piggery farm management systems have advanced significantly in recent years, using technology to enhance efficiency, productivity, and sustainability. These systems integrate various components such as automated feeding, environmental control, and health monitoring, offering a comprehensive approach to modern pig farming.</p>
 
     <p>Automated feeding systems have become more sophisticated, ensuring precise and consistent feed delivery. They emphasized that automated feeding not only reduces labor costs but also enhances growth performance and welfare in pigs by keeping consistent feeding schedules and quantities. Additionally, these systems can be customized to meet the specific nutritional needs of different pig groups, such as sows, piglets, and fattening pigs (van der Staay et.al, 2020).</p>
+    <p>Located at Kangwayan, Madridejos, Cebu</p>
+    <p></p>
 </div>
 
 <?php
 require 'users/footer.php';
  ?>
- <style>
-  body {
-    background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.4)), url('https://jsvpiggery.com/users/pages/images/piggery.jpg');
-    background-size: cover; 
-    background-position: center; 
-}
+ 
