9d65f17c2289fd9e5b204713f19c5f422fe2ada0
chadrhino <chadrhino29@gmail.com>
2024-12-13 17:04:22 +0800

    sd

--- sms_pass.php
+++ sms_pass.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once 'conn.php';
+require_once 'test.php';
 
 // Ensure the user has verified their OTP before proceeding
 if (!isset($_SESSION['otp_verified']) || $_SESSION['otp_verified'] !== true) {
