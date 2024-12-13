789cdedf2bc1853038a66188e66163939240118a
chadrhino <chadrhino29@gmail.com>
2024-12-13 17:42:36 +0800

    sdf

--- inc/modal-delete.php
+++ inc/modal-delete.php
@@ -1,30 +1,23 @@
 <div id="_remove" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	<div class="modal-dialog">
-
-	  <div class="modal-content">
-	     <div class="modal-header">
-		   <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
-		   <h3 class="modal-title">Remove From Quarantine List ?</h3>
-	    </div>
-		
-		<!-- <div class="modal-content"> -->
-	     <!-- <div class="modal-header">
-		   <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
-		   <h3 class="modal-title">Remove From Sold List ?</h3>
-	    </div> -->
-
-	   <div class="modal-body">
-		  <div class="alert alert-danger">
-		     <p>Are you sure you want to remove pig from quarantine list?.</p>
-		  </div>
-       </div>
-	   <div class="modal-footer">
-		  <button class="btn" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i> Close</button>
-		  <button type="submit" name="remove" class="btn btn-danger">Remove</button>
-	   </div>
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+          <h3 class="modal-title">Remove From Quarantine List?</h3>
+        </div>
+        <div class="modal-body">
+          <div class="alert alert-danger">
+            <p>Are you sure you want to remove pig from quarantine list?</p>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button class="btn" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i> Close</button>
+          <!-- This button submits the form and triggers the delete -->
+          <button type="submit" name="remove" class="btn btn-danger">Remove</button>
+        </div>
+      </div>
     </div>
   </div>
-</div>
 
 <div id="_remove" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
--- remove_quarantine.php
+++ remove_quarantine.php
@@ -1,6 +1,6 @@
 <?php 
 include 'setting/system.php';
-include 'theme/head.php'; 
+include 'theme/head.php';
 
 if (isset($_POST['remove'])) {
     if (!empty($_POST['selector'])) {
@@ -19,6 +19,7 @@ if (isset($_POST['remove'])) {
                 error_log("Failed to update pig status for pig_id: " . $row['id']);
             }
         }
+        // Success notification and redirection
         echo "<script>
             const Toast = Swal.mixin({
                 toast: true,
@@ -35,11 +36,11 @@ if (isset($_POST['remove'])) {
                 icon: 'success',
                 title: 'Pig removed from quarantine successfully'
             }).then(() => {
-                window.location.href = 'manage_quarantine.php';  // Adjusted path
+                window.location.href = 'manage_quarantine.php'; // Ensure this URL is correct
             });
         </script>";
     } else {
-        header("location: manage_quarantine.php"); // Adjusted path
+        header("location: manage_quarantine.php"); // Redirect if no pigs were selected
     }
 }
 ?>
