ff5e65e744aec7e760521f5d27249627449c1597
chadrhino <chadrhino29@gmail.com>
2024-12-13 18:09:34 +0800

    hs

--- signup.php
+++ signup.php
@@ -2,44 +2,64 @@
 <?php include 'theme/head.php'; ?>
 
 <div class="container">
-    <div class="row" style="margin-top: 10%">
-
+    <div class="row" style="margin-top: 5%">
+        <div class="col-md-2 col-md-offset-4" style="width: 100%;max-width: 400px;padding: 20px;border: 1px solid #333;background-color: #fff;box-sizing: border-box;text-align: center;">
         <h3 class="text-center">Sign Up</h3><br>
-        <div class="col-md-2 col-md-offset-2">
-            <img src="img/pig.png" class="img img-responsive">
-        </div>
-        <div class="col-md-4">
             <form method="post" autocomplete="off">
-                <div class="form-group">
-                    <label class="control-label">Name</label>
-                    <input type="text" name="name" class="form-control input-sm" required>
-                </div>
-                <div class="form-group">
-                    <label class="control-label">Email</label>
-                    <input type="email" name="email" class="form-control input-sm" required>
+
+                <div class="wrap-input100 validate-input">
+                    <input class="input100" type="text" maxlength="18" name="name" placeholder="Enter your name" required>
+                    <span span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                        <i class="fa " aria-hidden="true"></i>
+                    </span>
                 </div>
 
-                <div class="form-group">
-                    <label class="control-label">Password</label>
-                    <div style="position: relative;">
-                        <input type="password" name="password" class="form-control input-sm" required>
-                        <i class="fa fa-eye " id="showPass" data-target="password" style="position: absolute; top: 7px; right:10px;cursor: pointer;"></i>
-                    </div>
+                <div class="wrap-input100 validate-input">
+                    <input class="input100" type="email" name="email" placeholder="Enter your email" required>
+                    <span span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                    </span>
                 </div>
 
-                <div class="form-group">
-                    <label class="control-label">Confirm Password</label>
-                    <div style="position: relative;">
-                        <input type="password" name="confirm" class="form-control input-sm" required>
-                        <i class="fa fa-eye " id="showPass2" data-target="confirm" style="position: absolute; top: 7px; right:10px;cursor: pointer;"></i>
-                    </div>
+                <div class="wrap-input100 validate-input">
+                    <input class="input100" type="password" name="password" placeholder="Enter your password" required>
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                    <i class="fa fa-lock" aria-hidden="true"></i>
+                    </span>
+                    <i id="showPass" class="fa fa-eye" style="position: absolute; right: 25px; top: 50%; transform: translateY(-50%); cursor: pointer;"></i>
                 </div>
 
-                <div style="display: flex; gap: 20px; justify-content: space-between; align-items: center;">
-                    <button name="submit" type="submit" class="btn btn-md btn-dark">Submit</button>
-                    <!-- <a href="forgot.php">Forgot Password</a> -->
+                <div class="wrap-input100 validate-input">
+                    <input class="input100" type="password" name="confirm" placeholder="Enter confirm password"required>
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                    <i class="fa fa-lock" aria-hidden="true"></i>
+                    </span>
+                    <i id="showPass2" class="fa fa-eye" style="position: absolute; right: 25px; top: 50%; transform: translateY(-50%); cursor: pointer;"></i>
+                </div>
+
+                <div class="g-recaptcha" 
+          data-sitekey="6LeuvIsqAAAAABJsgpWNlWrB9vBl1dwI8DpUcZlr"
+          data-callback="recaptchaCallback">
+        </div>
+      <br>
+      <div class="wrap-input100 validate-input" style="text-align: left; margin-top: 20px;">
+                    <label>
+                        <input type="checkbox" name="terms" required>
+                        I agree to the <a href="terms.php" target="_blank">Terms and Conditions</a>.
+                    </label>
+                </div>
+                <div class="container-login100-form-btn">
+                    <button class="login100-form-btn" name="submit" type="submit" id="Button" disabled>
+                       Submit
+                    </button>
+                </div>
+
+
                     <p>Already have an account? <a href="login.php">Login</a></p>
-                </div>
 
 
             </form>
@@ -276,6 +296,12 @@
     </div>
 </div>
 
+
+
+
+<p class="text-center dont-print" style="margin-top: 7%; color: white;">
+	&copy; All Rights Reserved Chad Rhino Quijano 2024
+</p>
 <script>
     let password = document.querySelector("input[name='password']")
     let showPass = document.getElementById("showPass")
@@ -302,7 +328,81 @@
             showPass.classList.replace("fa-eye-slash", "fa-eye")
         }
     }
+    function recaptchaCallback() {
+    document.getElementById("Button").disabled = false;
+  }
 </script>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<style>
+  body {
+    background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.4)), url('https://jsvpiggery.com/users/pages/images/piggery.jpg');
+    background-size: cover; 
+    background-position: center; 
+}
+    .wrap-input100 {
+    position: relative;
+    width: 100%;
+    margin-bottom: 20px;
+  }
 
+  .input100 {
+    font-family: Poppins-Regular;
+    font-size: 16px;
+    color: #333;
+    line-height: 1.2;
+    display: block;
+    width: 100%;
+    background: #e6e6e6;
+    height: 55px;
+    border-radius: 25px;
+    padding: 0 30px 0 68px;
+    box-sizing: border-box;
+  }
 
-<?php include 'theme/foot.php'; ?>
+  .symbol-input100 {
+    position: absolute;
+    font-size: 18px;
+    color: #999999;
+    top: 50%;
+    left: 35px;
+    transform: translateY(-50%);
+    transition: all 0.4s;
+  }
+  .txt1 {
+    font-size: 14px;
+    color: #999999;
+    line-height: 1.5;
+  }
+
+  .txt2 {
+
+    font-size: 14px;
+    color: #333;
+    text-decoration: none;
+  }
+  .login100-form-btn {
+    font-family: Poppins-Medium;
+    font-size: 16px;
+    color: white;
+    background-color: #333;
+    border: none;
+    border-radius: 25px;
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 25px;
+    transition: all 0.4s;
+    cursor: pointer;
+  }
+  .g-recaptcha {
+    display: block; 
+    margin: 10px auto; 
+    box-sizing: border-box;
+    width: 302px;
+}
+  .p{
+    color: white;
+  }
+</style>
