b68ca4c0be8d5d3bf3d40618ebeff298319b92dc
chadrhino <chadrhino29@gmail.com>
2024-12-13 17:19:25 +0800

    sdfdsf

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-:<?php 
+<?php 
 include 'setting/system.php';
 include 'theme/head.php';
 // Start the session
@@ -11,6 +11,7 @@ if (isset($_SESSION['USER_ID']) && isset($_SESSION['USER_NAME']) && isset($_SESS
   echo "<script>window.location.href = 'dashboard.php';</script>";
   exit();
 }
+
 function handleFailedAttempt() {
   $_SESSION['login_attempts'] += 1;
 
@@ -33,83 +34,58 @@ function handleFailedAttempt() {
       </script>";
   }
 }
-			// Initialize attempt tracking
-      if (!isset($_SESSION['login_attempts'])) {
-        $_SESSION['login_attempts'] = 0;
-        $_SESSION['lockout_time'] = 0;
-      }
-      
-      $lockout_duration = 180; // Lockout duration in seconds (3 minutes)
-      
-      if (time() < $_SESSION['lockout_time']) {
-        $remaining_time = $_SESSION['lockout_time'] - time();
-        echo "<script>
-            Swal.fire({ 
-                icon: 'error', 
-                title: 'Too many attempts. Please wait $remaining_time seconds.' 
-            });
-        </script>";
-        exit();
-      }
-?>
+// Initialize attempt tracking
+if (!isset($_SESSION['login_attempts'])) {
+  $_SESSION['login_attempts'] = 0;
+  $_SESSION['lockout_time'] = 0;
+}
 
-<div class="container">
-	<div class="row" style="margin-top: 7% ">
-		<div class="col-md-2 col-md-offset-4" style="width: 100%;max-width: 400px;padding: 20px;border: 1px solid #333;background-color: #fff;box-sizing: border-box;text-align: center;">
-    <img src="img/pig.png" alt="Jsv" style="width: 120px; height: auto; margin-bottom: 20px;">
+$lockout_duration = 180; // Lockout duration in seconds (3 minutes)
 
-    <h1 class="text-center"><?php echo NAME_X; ?></h1><br>
-			<form method="post" autocomplete="off">
-
-        <div class="wrap-input100 validate-input">
-          <input class="input100" type="email" name="username" placeholder="Enter your email" required>
-          <span class="focus-input100"></span>
-          <span class="symbol-input100">
-            <i class="fa fa-envelope" aria-hidden="true"></i>
-          </span>
-        </div>
-
-          <div class="wrap-input100 validate-input">
-            <input class="input100" type="password" name="password" placeholder="Enter your password">
-            <span class="focus-input100"></span>
-            <span class="symbol-input100">
-              <i class="fa fa-lock" aria-hidden="true"></i>
-            </span>
-              <i id="showPass" class="fa fa-eye" style="position: absolute; right: 25px; top: 50%; transform: translateY(-50%); cursor: pointer;"></i>
-          </div>
-
-          <div class="g-recaptcha" 
-          data-sitekey="6LeuvIsqAAAAABJsgpWNlWrB9vBl1dwI8DpUcZlr"
-          data-callback="recaptchaCallback">
-        </div>
-      <br>
-
-          <div class="container-login100-form-btn">
-            <button class="login100-form-btn" name="submit" type="submit" id="Button" >
-              Login
-            </button>
-          </div>
-                
-          <div class="text-center p-t-12">
-            <a class="txt2" href="index.php">
-              Back To Home
-            </a>
-            <span class="txt1">
-            Don't have an account?
-            </span>
-            <a class="txt2" href="signup.php">
-            Sign Up
-            </a>
-          </div> 
-          <a href="forgot_portal.php">Forgot Password</a>
-			</form>
-
-			<?php
-
+if (time() < $_SESSION['lockout_time']) {
+  $remaining_time = $_SESSION['lockout_time'] - time();
+  echo "<script>
+      Swal.fire({ 
+          icon: 'error', 
+          title: 'Too many attempts. Please wait $remaining_time seconds.' 
+      });
+  </script>";
+  exit();
+}
 
 if (isset($_POST['submit'])) {
   $username = htmlspecialchars(stripslashes(trim($_POST['username'])));
   $password = htmlspecialchars(stripslashes(trim($_POST['password'])));
+  $recaptcha_response = $_POST['g-recaptcha-response'];
+
+  // Verify reCAPTCHA response with Google
+  $secret_key = "6LerfZoqAAAAAOezcowKEPp1SlkdAYfrXuIQvJTl"; // Replace with your reCAPTCHA secret key
+  $url = "https://www.google.com/recaptcha/api/siteverify";
+  $data = [
+    'secret' => $secret_key,
+    'response' => $recaptcha_response
+  ];
+
+  $options = [
+    'http' => [
+      'method'  => 'POST',
+      'content' => http_build_query($data),
+      'header'  => "Content-Type: application/x-www-form-urlencoded\r\n"
+    ]
+  ];
+  $context = stream_context_create($options);
+  $verify = file_get_contents($url, false, $context);
+  $captcha_success = json_decode($verify);
+
+  if (!$captcha_success->success) {
+    echo "<script>
+        Swal.fire({ 
+            icon: 'error', 
+            title: 'Please verify that you are not a robot.' 
+        });
+    </script>";
+    exit();
+  }
 
   // Check admin table first
   $get_admin = $db->prepare("SELECT * FROM admin WHERE username = :uname");
@@ -158,15 +134,62 @@ if (isset($_POST['submit'])) {
   }
 }
 ?>
-		</div>
-	</div>
+
+<div class="container">
+  <div class="row" style="margin-top: 7%">
+    <div class="col-md-2 col-md-offset-4" style="width: 100%;max-width: 400px;padding: 20px;border: 1px solid #333;background-color: #fff;box-sizing: border-box;text-align: center;">
+      <img src="img/pig.png" alt="Jsv" style="width: 120px; height: auto; margin-bottom: 20px;">
+      <h1 class="text-center"><?php echo NAME_X; ?></h1><br>
+      <form method="post" autocomplete="off" id="loginForm">
+        <div class="wrap-input100 validate-input">
+          <input class="input100" type="email" name="username" placeholder="Enter your email" required>
+          <span class="focus-input100"></span>
+          <span class="symbol-input100">
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+          </span>
+        </div>
+
+        <div class="wrap-input100 validate-input">
+          <input class="input100" type="password" name="password" placeholder="Enter your password">
+          <span class="focus-input100"></span>
+          <span class="symbol-input100">
+            <i class="fa fa-lock" aria-hidden="true"></i>
+          </span>
+          <i id="showPass" class="fa fa-eye" style="position: absolute; right: 25px; top: 50%; transform: translateY(-50%); cursor: pointer;"></i>
+        </div>
+
+        <div class="g-recaptcha" data-sitekey="6LerfZoqAAAAAFVjWq9xPCoxloo8aQRwyay5xzBT" data-callback="recaptchaCallback"></div>
+        <br>
+
+        <div class="container-login100-form-btn">
+          <button class="login100-form-btn" name="submit" type="submit" id="Button" disabled>
+            Login
+          </button>
+        </div>
+
+        <div class="text-center p-t-12">
+          <a class="txt2" href="index.php">
+            Back To Home
+          </a>
+          <span class="txt1">
+            Don't have an account?
+          </span>
+          <a class="txt2" href="signup.php">
+            Sign Up
+          </a>
+        </div>
+        <a href="forgot_portal.php">Forgot Password</a>
+      </form>
+
+    </div>
+  </div>
 </div>
 
 <p class="text-center dont-print" style="margin-top: 6%; color: white;">
-	&copy; All Rights Reserved Chad Rhino Quijano 2024
+  &copy; All Rights Reserved Chad Rhino Quijano 2024
 </p>
- <!-- <script src="https://www.google.com/recaptcha/api.js" async defer></script> -->
 
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
 <script>
   let password = document.querySelector("input[name='password']");
@@ -181,81 +204,12 @@ if (isset($_POST['submit'])) {
       showPass.classList.replace("fa-eye", "fa-eye-slash");
     }
   }
-	//   function recaptchaCallback() {
-  //   document.getElementById("Button").disabled = false;
-  // }
+
+  function recaptchaCallback() {
+    document.getElementById("Button").disabled = false;
+  }
 </script>
 
 <style>
-  body {
-    background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.4)), url('https://jsvpiggery.com/users/pages/images/piggery.jpg');
-    background-size: cover; 
-    background-position: center; 
-}
-.g-recaptcha {
-    display: block; 
-    margin: 10px auto; 
-    box-sizing: border-box;
-    width: 302px;
-}
-    .wrap-input100 {
-    position: relative;
-    width: 100%;
-    margin-bottom: 20px;
-  }
-
-  .input100 {
-    font-family: Poppins-Regular;
-    font-size: 16px;
-    color: #333;
-    line-height: 1.2;
-    display: block;
-    width: 100%;
-    background: #e6e6e6;
-    height: 55px;
-    border-radius: 25px;
-    padding: 0 30px 0 68px;
-    box-sizing: border-box;
-  }
-
-  .symbol-input100 {
-    position: absolute;
-    font-size: 18px;
-    color: #999999;
-    top: 50%;
-    left: 35px;
-    transform: translateY(-50%);
-    transition: all 0.4s;
-  }
-  .txt1 {
-    font-size: 14px;
-    color: #999999;
-    line-height: 1.5;
-  }
-
-  .txt2 {
-    font-size: 14px;
-    color: #333;
-    text-decoration: none;
-  }
-  
-  .login100-form-btn {
-    font-family: Poppins-Medium;
-    font-size: 16px;
-    color: white;
-    background-color: #333;
-    border: none;
-    border-radius: 25px;
-    width: 100%;
-    height: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0 25px;
-    transition: all 0.4s;
-    cursor: pointer;
-  }
-  .footer{
-    color: white;
-  }
+  /* Your existing styles here */
 </style>
