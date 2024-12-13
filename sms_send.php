200220b692a0636b3964e7bfce7dfe23592bd08b
chadrhino <chadrhino29@gmail.com>
2024-12-13 16:33:05 +0800

    sdsasdasd

--- sms_send.php
+++ sms_send.php
@@ -1,87 +1,6 @@
 <?php
 
-session_start();
-
-$servername = "127.0.0.1";
-$username = "u510162695_pig"; 
-$password = "1Pigdatabase"; 
-$dbname = "u510162695_pig"; 
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-class SMSGateway {
-    private $apiUrl;
-    private $apiKey;
-
-
-    public function __construct() {
-        $this->apiUrl = 'https://qdkjr3.api.infobip.com/sms/2/text/advanced';
-        $this->apiKey = '7fdb0119147f3b81ac059f3ce82b23ca-1ff36143-73d5-4cef-be74-58da526fb1f2';
-    }
-
-    public function sendSMS($phone, $message) {
-        try {
-            $data = [
-                "messages" => [
-                    [
-                        "destinations" => [
-                            ["to" => $phone]
-                        ],
-                        "text" => $message
-                    ]
-                ]
-            ];
-
-            $ch = curl_init();
-            curl_setopt_array($ch, [
-                CURLOPT_URL => $this->apiUrl,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_POST => true,
-                CURLOPT_POSTFIELDS => json_encode($data),
-                CURLOPT_HTTPHEADER => [
-                    'Authorization: App ' . $this->apiKey,
-                    'Content-Type: application/json'
-                ]
-            ]);
-
-            $response = curl_exec($ch);
-
-            if (curl_errno($ch)) {
-                error_log('Infobip SMS Error: ' . curl_error($ch));
-                return false;
-            }
-
-            curl_close($ch);
-
-            $result = json_decode($response, true);
-            if (isset($result['messages'][0]['status']['groupName']) &&
-                $result['messages'][0]['status']['groupName'] === "PENDING") {
-                return true;
-            }
-
-            error_log('Infobip SMS Error: ' . $response);
-            return false;
-        } catch (Exception $e) {
-            error_log('Infobip SMS Exception: ' . $e->getMessage());
-            return false;
-        }
-    }
-}
-
-function sendSMS($phone, $message) {
-    $smsGateway = new SMSGateway();
-
-    // Ensure correct format: +63XXXXXXXXX
-    if (substr($phone, 0, 1) === '0') {
-        $phone = '+63' . substr($phone, 1);
-    }
-
-    return $smsGateway->sendSMS($phone, $message);
-}
+include 'conn.php';
 
 
 
@@ -183,7 +102,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             
             // If we got here, everything worked
             $_SESSION['success_message'] = "OTP sent successfully! Please check your phone.";
-            header("Location:send_otp.php");
+            header("Location:send_otp");
             exit();
             
         } else {
@@ -336,7 +255,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </form>
 
         <div class="login">
-            <a href="../login.php">Back to Login</a>
+            <a href="login.php">Back to Login</a>
         </div>
     </div>
     <script>
