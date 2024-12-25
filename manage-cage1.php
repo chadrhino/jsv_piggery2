7e74374f3d05608f5275dd970c094c0564fe18e9
chadrhino <chadrhino29@gmail.com>
2024-12-24 19:22:06 -0800

    commit

--- manage-cage1.php
+++ manage-cage1.php
@@ -45,7 +45,7 @@
           </thead>
           <tbody>
             <?php
-            $all_pig = $db->query("SELECT p.*,b.name AS breed_name, c.name AS class_name, v.name AS vita_name, f.name AS feed_name  FROM pigs p LEFT JOIN breed b ON p.breed_id = b.id LEFT JOIN vitamins v ON p.vitamins_id = v.id LEFT JOIN classification c ON p.classification_id = c.id LEFT JOIN feed f ON p.feed_id = f.id WHERE p.status = 1 AND p.cage_num IS NULL AND p.type IS NULL ORDER BY p.id DESC ");
+            $all_pig = $db->query("SELECT p.*,b.name AS breed_name, c.name AS class_name, v.name AS vita_name, f.name AS feed_name  FROM pigs p LEFT JOIN breed b ON p.breed_id = b.id LEFT JOIN vitamins v ON p.vitamins_id = v.id LEFT JOIN classification c ON p.classification_id = c.id LEFT JOIN feed f ON p.feed_id = f.id WHERE p.status = 1 AND p.cage_num = 1 AND p.type IS NULL ORDER BY p.id DESC ");
             $fetch = $all_pig->fetchAll(PDO::FETCH_OBJ);
             foreach ($fetch as $data) {
               // $get_breed = $db->query("SELECT * FROM breed WHERE id = '$data->breed_id'");
