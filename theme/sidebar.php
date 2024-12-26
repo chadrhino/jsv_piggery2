c2153cea97c91c4e3076b7fb7a50db468ba90295
chadrhino <chadrhino29@gmail.com>
2024-12-26 20:10:30 +0800

    ur

--- theme/sidebar.php
+++ theme/sidebar.php
@@ -12,7 +12,7 @@ $active = "w3-blue";
 <nav class="w3-sidebar w3-collapse w3-white w3-animate-left dont-print" style="z-index:3;width:300px;" id="mySidebar"><br>
   <div class="w3-container w3-row">
     <div class="w3-col s4">
-      <img src="img/pig.png" class="w3-circle w3-margin-right" style="width:46px">
+      <img src="img/pig14.jpg" class="w3-circle w3-margin-right" style="width:46px">
     </div>
     <div class="w3-col s8 w3-bar">
       <span>Welcome, <strong><?php echo ucwords($_SESSION['name']); ?></strong></span><br>
